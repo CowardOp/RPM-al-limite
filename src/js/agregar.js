2b056bbb577faa5e0a012d7566febf056726646d
Brayan Casallas <beto980730@gmail.com>
2024-09-11 11:40:54 -0500

    New

--- src/js/agregar.js
+++ src/js/agregar.js
@@ -1,44 +1,29 @@
 urlServicio = "http://localhost:8081/api/products";
 let agregarProductosNuevos = document.getElementById("agregarProductosNuevos");
-// const agregarProductos = () => {
-//   console.log("Agregando productos");
-//   var data = [];
-//   var success = function (response) {
-//     console.log(response);
-//     agregarProductosNuevos.innerHTML = response;
-//   };
-
-//   $.ajax({
-//     type: "GET",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-type": "application/jason",
-//     },
-//     url: urlServicio,
-//     context: data,
-//     dataType: "json",
-//     success: success,
-//   });
-// };
-
-// agregarProductos();
 
 const otroProducto = async () => {
   try {
     const response = await fetch(urlServicio);
     if (!response.ok) {
-      throw new error(`Data response was not ok ${response.statusText}`);
+      throw new Error(`Data response was not ok ${response.statusText}`);
     }
     const data = await response.json();
-    console.log(data);
-    agregarProductosNuevos.innerHTML = `<th>${data[0].name}</th>
-    <th>${data[0].price}</th> 
-    <th>${data[0].stock}</th>
-    <th>${data[0].categoryId.name}</th>
-    <th>${data[0].unityId.name}</th>
-    <th><button class="btn btn-xs btn-accent">Editar</button></th>
-    <th><button class="btn btn-xs btn-error">Eliminar</button></th>
-    `;
+
+    agregarProductosNuevos.innerHTML = "";
+
+    data.forEach((producto) => {
+      agregarProductosNuevos.innerHTML += `
+        <tr>
+          <th>${producto.name}</th>
+          <th>${producto.price}</th> 
+          <th>${producto.stock}</th>
+          <th>${producto.categoryId.name}</th>
+          <th>${producto.unityId.name}</th>
+          <th><button class="btn btn-xs btn-accent">Editar</button></th>
+          <th><button class="btn btn-xs btn-error">Eliminar</button></th>
+        </tr>
+      `;
+    });
   } catch (e) {
     console.log(`Error ${e}`);
   }
