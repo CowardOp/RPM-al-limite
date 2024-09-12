7c2a8389726d51f273c3521dba281e2ffb22a452
Brayan Casallas <beto980730@gmail.com>
2024-09-11 20:59:56 -0500

    update

--- src/js/agregar.js
+++ src/js/agregar.js
@@ -1,24 +1,28 @@
-urlServicio = "http://localhost:8081/api/products";
+urlServicioProducts = "http://localhost:8081/api/products";
+urlServicioCategories = "http://localhost:8081/api/categories";
+urlServicioUnity = "http://localhost:8081/api/unity";
 let agregarProductosNuevos = document.getElementById("agregarProductosNuevos");
+let nuevaCategoria = document.getElementById("categoryId");
+let nuevaUnidad = document.getElementById("unityId");
 
 const otroProducto = async () => {
   try {
-    const response = await fetch(urlServicio);
+    const response = await fetch(urlServicioProducts);
     if (!response.ok) {
-      throw new Error(`Data response was not ok ${response.statusText}`);
+      throw new Error(`Data response no esta ok ${response.statusText}`);
     }
     const data = await response.json();
 
     let items = [];
 
-    data.forEach((producto) => {
+    data.forEach((product) => {
       items.push(`
         <tr>
-          <th class="text-xl">${producto.name}</th>
-          <th class="text-xl">${producto.price}</th> 
-          <th class="text-xl">${producto.stock}</th>
-          <th class="text-xl">${producto.categoryId.name}</th>
-          <th class="text-xl">${producto.unityId.name}</th>
+          <th class="text-xl">${product.name}</th>
+          <th class="text-xl">${product.price}</th> 
+          <th class="text-xl">${product.stock}</th>
+          <th class="text-xl">${product.categoryId.name}</th>
+          <th class="text-xl">${product.unityId.name}</th>
           <th class="flex justify-center"><button class="btn btn-xs btn-accent">Editar</button><button class="btn btn-xs btn-error">Eliminar</button></th>
         </tr>
       `);
---
+++ src/js/nuevo.js
@@ -0,0 +1,87 @@
+urlServicioCategories = "http://localhost:8081/api/categories";
+urlServicioUnity = "http://localhost:8081/api/unity";
+urlServicioProducts = "http://localhost:8081/api/products";
+
+let nuevaCategoria = document.getElementById("categoryId");
+let nuevaUnidad = document.getElementById("unityId");
+
+const newCategories = async () => {
+  try {
+    const response = await fetch(urlServicioCategories);
+    if (!response.ok) {
+      throw new Error(`Data response no esta ok ${response.statusText}`);
+    }
+    const data = await response.json();
+    console.log(data);
+
+    let itemCategory = [];
+
+    data.forEach((category) => {
+      itemCategory.push(`<option class="text-white">${category.name}</option>`);
+    });
+    nuevaCategoria.innerHTML =
+      `<option class="text-white" disabled selected> Seleccione una unidad</option>` +
+      itemCategory.join("");
+  } catch (e) {
+    console.log(`Error ${e}`);
+  }
+};
+
+newCategories();
+
+const newUnity = async () => {
+  try {
+    const response = await fetch(urlServicioUnity);
+    if (!response.ok) {
+      throw new Error(`Data response no esta ok ${response.statusText}`);
+    }
+    const data = await response.json();
+    console.log(data);
+
+    let itemUnity = [];
+
+    data.forEach((unity) => {
+      itemUnity.push(`<option class="text-white">${unity.name}</option>`);
+    });
+    nuevaUnidad.innerHTML =
+      `<option class="text-white" disabled selected> Seleccione una unidad</option>` +
+      itemUnity.join("");
+  } catch (e) {
+    console.log(`Error ${e}`);
+  }
+};
+
+newUnity();
+
+const nuevoProducto = () => {
+  console.log("nuevo producto");
+
+  var data = convertirFormDataAJSON($("#formNuevo"));
+
+  var success = (response) => {
+    alert("El producto fue agregado");
+    window.location.href = "agregar.html";
+  };
+
+  $.ajax({
+    type: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    url: urlServicioProducts,
+    data: data,
+    dataType: "json",
+    success: success,
+  });
+};
+
+const convertirFormDataAJSON = (formData) => {
+  let loginForm = formData.serializeArray();
+  let loginObject = {};
+
+  $.each(loginForm, function (i, v) {
+    loginObject[v.name] = v.value;
+  });
+  return JSON.stringify(loginObject);
+};
